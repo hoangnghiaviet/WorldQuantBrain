20cf5fead1ad856624cb76b8c3807431e6c342f9
Hoang Nghia Viet <viethoangnghia@gmail.com>
2023-04-02 15:48:59 +0700

    second commit

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.markdown('Streamlit is **_really_ cool**.')
 st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")
-st.markdown("Given an array size $n$: $a_1, a_2, ..., a_n$. Calculate the sum of this array")
+st.markdown("Ditme thang Thien")
